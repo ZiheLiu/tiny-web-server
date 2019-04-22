00105ac3e8a2b4fb1af331e347011ba16bc4db68
ZiheLiu <272910663@qq.com>
2019-04-22 22:24:33 +0800

    Reconstructure project with cmake

--- 11.6/tiny.c
+++ 11.6/tiny.c
@@ -1,21 +1,7 @@
-#include "csapp.h"
+#include "../csapp.h"
 
-#include <signal.h>
+#include "tiny.h"
 
-
-void doit(int fd);
-
-void read_requesthdrs(rio_t *rp);
-
-int parse_uri(char *uri, char *filename, char *cgiargs);
-
-void serve_static(int fd, char *filename, int filesize);
-
-void get_filetype(char *filename, char* filetype);
-
-void serve_dynamic(int fd, char *filename, char* cgiargs);
-
-void clienterror(int fd, char *cause, char *errnum, char *shortmsg, char *longmsg);
 
 int main(int argc, char **argv) {
   int listenfd, connfd;
@@ -35,14 +21,8 @@ int main(int argc, char **argv) {
     connfd = Accept(listenfd, (SA *) &clientaddr, &clientlen);
     Getnameinfo((SA *) &clientaddr, clientlen, hostname, MAXLINE, port, MAXLINE, 0);
     printf("Accepted connection from (%s, %s)\n", hostname, port);
-//    doit(connfd);
-    if (Fork() == 0) {
-      Close(listenfd);
-      doit(connfd);                                             //line:netp:tiny:doit
-      Close(connfd);                                            //line:netp:tiny:close
-      exit(0);
-    }
 
+    doit(connfd);
     Close(connfd);
   }
 }
